44cd8d05149024c465168532361f253f2be44be9
rudraskhkarpe <rudraksh.karpe.cs@ghrcem.raisoni.net>
2022-07-08 05:47:28 +0530

    updated text mesasges

--- lib/data.dart
+++ lib/data.dart
@@ -7,8 +7,8 @@ class Data {
     Message(isMe: false, message: "'How're you?"),
     Message(isMe: true, message: 'I\' fine what about you?'),
     Message(isMe: false, message: 'Well, there\'s an intersting update about our Translation App!'),
-    Message(isMe: true, message: 'Yes, I love travelling'),
-    Message(isMe: false, message: 'Lets go on a trip'),
-    Message(isMe: true, message: 'Cool, where are we going?'),
+    Message(isMe: true, message: 'We\'ll talk later I\m travelling right now!'),
+    Message(isMe: false, message: 'Alright, see you later!'),
+    Message(isMe: true, message: 'yes, for sure!'),
   ];
 }
