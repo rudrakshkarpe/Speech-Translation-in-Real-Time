73228e78fabbd246e2dd44a85a2c15269dfd5ccf
rudrakshkarpe <rudraksh.karpe@gmail.com>
2022-09-07 19:47:19 +0530

    updated widget-test.adrt

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,6 +14,7 @@ import 'package:flutter_test/flutter_test.dart';
 import "package:speech_transaltion_in_realtime/main.dart";
 
 void main() {
+  
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
@@ -30,4 +31,5 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
 }
